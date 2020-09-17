215d04090d60fe5222caeb8f2720613e871588cb
ArkadiuszPrzybysz <70707115+ArkadiuszPrzybysz@users.noreply.github.com>
2020-09-17 16:37:01 +0200

    Add files via upload

--- 2_operacje_na_zmiennych.php
+++ 2_operacje_na_zmiennych.php
@@ -31,6 +31,83 @@ echo"$x<br>"; //101(2) => 1+4=5
   echo $x <=> $y;
   $result =  $x <=> $y;
   echo $x <=> $y;
+  $x=10;
+  $y=10;
+
+
+  if ($x==$y)
+  {
+    echo 'zmienne sa rowne<br>';
+
+  } else{
+    echo 'zmienne nie sa rowne<br>';
+  }
+
+  echo gettype($x); //intiger
+  echo gettype($y),'<hr>'; //double
+
+
+
+
+  //  preinkrementacja ++$
+  //  predekrementacja --$x
+//    postinkrementacja $x++
+//    postdekrementacja $x--
+
+
+  $x=2;
+  echo $x; //2
+  $x=$x--;
+  $x=$x+3;
+  echo $x; //5
+  $x=$x++;
+  echo $x; //5
+  $x=++$x;
+  echo $x, '<br>'; //6
+  $y=++$x;
+  $y=++$x*2+3;
+  echo $x, '<br>'; //
+  echo $y; //
+
+
+
+  //zadanie
+
+
+  $x=2;
+  echo $x++, '<br>';//2
+  echo ++$x, '<br>';//4
+  echo $x, '<br>';//4
+  $y=$x++;
+  echo $y, '<br>';//4
+  $y=++$x;
+  echo $y, '<br>';//6
+  echo ++$y, '<br>';//7
+
+
+
+  //opertory rzutowania
+
+  //bool, int, float, string, unset, array, object,
+
+
+
+  $text='123abc';
+  $text=0;
+  $text2=20;
+
+
+echo 'Typ danych $text: ', gettype($text), '<br>';
+$x=(int)$text;
+echo $x;
+echo '<br>Typ danych $x: ', gettype($x), '<br>';
+echo '<br Typ danych $text: ', gettype($text), '<br>';
+
+
+$x=(bool)$text1;
+echo $x; //false
+
+
 
 
 
